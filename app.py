b6da7346b50730280660b94cc7f5fe7285ac2580
Ed1196 <34897031+Ed1196@users.noreply.github.com>
2020-02-22 17:59:15 -0500

    Initial set-up, Simple Hello World endpoint.

---
+++ app.py
@@ -0,0 +1,22 @@
+"""
+Necessary Modules
+    Flask is a framework.
+    Flask_Restful is an extension to Flask
+"""
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+
+# Adds the resource to our API
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
